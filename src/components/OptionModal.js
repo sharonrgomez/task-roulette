b9743da59010b70cac8fa2893fb399294dde1a21
Sharon <srcbun@gmail.com>
2020-05-07 18:52:46 -0400

    make app mobile friendly

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,14 +3,21 @@ import Modal from "react-modal";
 
 const OptionModal = (props) => (      // can use implicit return, don't need to explicitly say { return (<div></div>) }
     <Modal
+        style={{
+            overlay: {
+                backgroundColor: "rgba(255, 255, 255, 0.4)"
+            }
+        }}
+        className="modal"
         isOpen={!!props.selectedOption}         // !! converts value to boolean
         contentLabel="Selected Option"
         onRequestClose={props.handleClearSelectedOption}
         appElement={app}
+        closeTimeoutMS={175}
     >
-        <h1>Selected Option</h1>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClearSelectedOption}>Close window</button>
+        <h3>Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleClearSelectedOption}>Close</button>
     </Modal>
 );
 
